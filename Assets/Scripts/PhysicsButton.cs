60cca08f8db5d738095f49ce3b929b5c2b85478f
ZSZglogowVR <71322576+ZSZglogowVR@users.noreply.github.com>
2021-09-28 10:32:58 +0200

    version 1.0

---
+++ Assets/Scripts/PhysicsButton.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using Valve.VR.InteractionSystem;
+
+public class PhysicsButton : MonoBehaviour
+{
+    [SerializeField] private float threshold = .1f;
+    [SerializeField] private float deadZone = .025f;
+
+    private bool _isPressed;
+    private Vector3 _startPos;
+    private ConfigurableJoint _joint;
+
+    public Transform spawnPos;
+    public GameObject spawnee;
+    GameObject clone;
+
+
+    void Start()
+    {
+        _startPos = transform.localPosition;
+        _joint = GetComponent<ConfigurableJoint>();
+    }
+
+    void Update()
+    {
+        if (!_isPressed && GetValue() + threshold >= 1) Pressed();
+        if (_isPressed && GetValue() - threshold <= 0) Released();
+    }
+    private float GetValue()
+    {
+        var value = Vector3.Distance(_startPos, transform.localPosition) / _joint.linearLimit.limit;
+
+        if (Mathf.Abs(value) < deadZone) value = 0;
+        return Mathf.Clamp(value, -1f, 1f);
+    }
+    private void Pressed()
+    {
+        foreach (Hand hand in GameObject.FindObjectsOfType<Hand>())
+        {
+
+            GameObject gameObject = hand.currentAttachedObject;
+            if (gameObject!=null)
+            {
+                if (gameObject.Equals(clone))
+                {
+                    hand.DetachObject(gameObject, false);
+                }
+            }
+            Destroy(clone);
+        }
+        clone = (GameObject)Instantiate(spawnee, spawnPos.position, spawnPos.rotation);
+        _isPressed = true;
+    }
+
+    private void Released()
+    {
+        _isPressed = false;
+    }
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Valve.VR.InteractionSystem;
+public class ScoreManager : MonoBehaviour
+{
+    GameObject text;
+    public GameObject structure;
+    bool win = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GameObject.FindGameObjectWithTag("Text");
+        text.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        int count = 0;
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("Cylinder");
+        foreach (GameObject gameObject in objects)
+        {
+            Debug.Log("1" + Vector3.Dot(gameObject.transform.up, Vector3.right));
+            Debug.Log("2" + Vector3.Dot(gameObject.transform.up, Vector3.down));
+            Debug.Log("0" + Vector3.Dot(gameObject.transform.up, Vector3.left));
+            Debug.Log("3" + Vector3.Dot(gameObject.transform.up, Vector3.forward));
+            Debug.Log("4" + Vector3.Dot(gameObject.transform.up, Vector3.back));
+            if ((Vector3.Dot(gameObject.transform.up, Vector3.right) > 0.5f
+                || Vector3.Dot(gameObject.transform.up, Vector3.down) > 0
+                || Vector3.Dot(gameObject.transform.up, Vector3.left) > 0.5f
+                || Vector3.Dot(gameObject.transform.up, Vector3.forward) > 0.5f
+                || Vector3.Dot(gameObject.transform.up, Vector3.back) > 0.5f)||gameObject.transform.position.y<0.45)
+            {
+                count++;
+                if (count == objects.Length && !win)
+                {
+                    Debug.Log("win");
+                    StartCoroutine(waiter());
+                }
+            }    
+        }
+        
+    }
+    IEnumerator waiter()
+    {
+        Destroy(GameObject.FindGameObjectWithTag("Structure"));
+        win = true;
+        text.SetActive(true);
+        yield return new WaitForSecondsRealtime(4);
+        foreach (Hand hand in GameObject.FindObjectsOfType<Hand>())
+        {
+            GameObject gameObject = hand.currentAttachedObject;
+            hand.DetachObject(gameObject, false);
+            Destroy(gameObject);
+        }
+        text.SetActive(false);
+        SceneManager.LoadScene("MenuScene");
+    }
+}
